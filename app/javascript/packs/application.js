d8778e59ffff54cf230aff4d43815864ad496f0d
amrutsabale <amrut.sabale@bigbinary.com>
2021-06-11 11:20:12 +0530

    Added API based architecture

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -11,6 +11,6 @@ import "../stylesheets/application.scss";
 Rails.start();
 ActiveStorage.start();
 // Support component names relative to this directory:
-var componentRequireContext = require.context("components", true);
+var componentRequireContext = require.context("src", true);
 var ReactRailsUJS = require("react_ujs");
 ReactRailsUJS.useContext(componentRequireContext);
--- app/javascript/src/App.jsx
+++ app/javascript/src/App.jsx
@@ -1,20 +1,20 @@
 import React, { useEffect } from "react";
 import { Route, Switch, BrowserRouter as Router } from "react-router-dom";
+import { initializeLogger } from "./common/logger";
 
 const App = () => {
   useEffect(() => {
-    /*eslint no-undef: "off"*/
-    initializeLogger;
-    logger.info("Log from js-logger");
+    initializeLogger();
   }, []);
 
   return (
-    <Router>
-      <Switch>
-        <Route exact path="/" render={() => <div>Home</div>} />
-        <Route exact path="/about" render={() => <div>About</div>} />
-      </Switch>
-    </Router>
+    // <Router>
+    //   <Switch>
+    //     <Route exact path="/" render={() => <div>Home</div>} />
+    //     <Route exact path="/about" render={() => <div>About</div>} />
+    //   </Switch>
+    // </Router>
+    <h1>This is App.js</h1>
   );
 };
 
