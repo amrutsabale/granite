b4e3c6fac7b46d7fcc4a2659121ab92561db37ab
amrutsabale <amrut.sabale@bigbinary.com>
2021-06-16 14:34:33 +0530

    Added progress to tasks

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -4,7 +4,9 @@ class TasksController < ApplicationController
 
   def index
    tasks = policy_scope(Task)
-   render status: :ok, json: { tasks: tasks }
+   pending_tasks = tasks.pending
+   completed_tasks = tasks.completed
+   render status: :ok, json: { tasks: { pending: pending_tasks, completed: completed_tasks } }
   end
 
   def show
@@ -56,7 +58,7 @@ class TasksController < ApplicationController
   private
 
   def task_params
-    params.require(:task).permit(:title, :user_id,:authorize_owner)
+    params.require(:task).permit(:title, :user_id,:progress,:authorize_owner)
   end
 
   def load_task
--- app/javascript/src/components/Tasks/Table/index.jsx
+++ app/javascript/src/components/Tasks/Table/index.jsx
@@ -3,18 +3,27 @@ import React from "react";
 import TableHeader from "./TableHeader";
 import TableRow from "./TableRow";
 
-const Table = ({ data, destroyTask, updateTask, showTask }) => {
+const Table = ({
+  type = "pending",
+  data,
+  destroyTask,
+  showTask,
+  handleProgressToggle,
+  starTask,
+}) => {
   return (
     <div className="flex flex-col mt-10 ">
       <div className="my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
         <div className="inline-block min-w-full py-2 align-middle sm:px-6 lg:px-8">
           <div className="overflow-hidden border-b border-gray-200 shadow md:custom-box-shadow">
             <table className="min-w-full divide-y divide-gray-200">
-              <TableHeader />
+              <TableHeader type={type} />
               <TableRow
                 data={data}
                 showTask={showTask}
-                updateTask={updateTask}
+                type={type}
+                handleProgressToggle={handleProgressToggle}
+                starTask={starTask}
                 destroyTask={destroyTask}
               />
             </table>
