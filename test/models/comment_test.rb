920b4ca14765954872abe18fadac4103a2ee5e15
amrutsabale <samrut355@gmail.com>
2021-06-24 12:18:07 +0530

    Updated setup method for Comment model tests

--- test/models/comment_test.rb
+++ test/models/comment_test.rb
@@ -2,13 +2,12 @@ require 'test_helper'
 
 class CommentTest < ActiveSupport::TestCase
   def setup
-    @user = User.create(name: 'Sam Smith',
-                        email: 'sam@example.com',
-                        password: 'welcome',
-                        password_confirmation: 'welcome')
-    @task = @user.tasks.new(title: 'This is a test task')
-    @comment = @task.comments.new(content: 'This is a test comment')
-    @comment.user = @user
+    @user = User.create!(name: 'Sam Smith',
+                         email: 'sam@example.com',
+                         password: 'welcome',
+                         password_confirmation: 'welcome')
+    @task = @user.tasks.create!(title: 'This is a test task')
+    @comment = @task.comments.new(content: 'This is a test comment', user: @user)
   end
 
   def test_comment_should_be_invalid_without_content
